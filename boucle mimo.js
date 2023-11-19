8cddaa8b309cb966e46404a36273e0cccab7129a
BINDAT <maximerigaud66@gmail.com>
2023-11-19 11:24:54 +0100

    Create boucle mimo.js

---
+++ boucle mimo.js
@@ -0,0 +1,9 @@
+const friends = ["Jane","Jim","Jeri"];
+for(let i =0; i < friends.length; i++){
+    console.log(friends[i]);
+}
+/*
+Observation : boucle creer grace a l'option for qui instaure une var i qui est egale 
+a 0 friends.length et plus grand que i ; qui par la suite s'auto incremente.
+ceci affichant le tableau 
+*/
