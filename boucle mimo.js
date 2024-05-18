5d74a59d1f5acdfb99f08f57687b06a2178975d4
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-18 20:46:38 +0200

    Update boucle mimo.js

--- boucle mimo.js
+++ boucle mimo.js
@@ -1,6 +1,6 @@
-const friends = ["Jane","Jim","Jeri"];
+const friends = ["Jane","Jim","Jeri"]; //définition de constance et de sa valeur
 for(let i =0; i < friends.length; i++){
-    console.log(friends[i]);
+    console.log(friends[i]); // boucle for qui prend la variable à la valeur 0 et compte.
 }
 /*
 Observation : boucle creer grace a l'option for qui instaure une var i qui est egale 
