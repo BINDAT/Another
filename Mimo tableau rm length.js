156e081fd974713e2d37b802ff88a504671e78a9
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-19 12:51:50 +0200

    Update Mimo tableau rm length.js

--- Mimo tableau rm length.js
+++ Mimo tableau rm length.js
@@ -1,4 +1,4 @@
-const todos = ["study","chores","meditate"] #création de la constante todos avec sa valeur.
-remove = todos.pop(); #Détruit la dernière valeur de todos
-const leftToDo = todos.length; #reprend la constante sous une autre constante pour ensuite la traduire en nombre.
-console.log(`You have ${leftToDo} items left to do.`);#affiche en nombre la constante leftToDo en nombre sur todos grâce 
+const todos = ["study","chores","meditate"] //création de la constante todos avec sa valeur.
+remove = todos.pop(); //Détruit la dernière valeur de todos
+const leftToDo = todos.length; //reprend la constante sous une autre constante pour ensuite la traduire en nombre.
+console.log(`You have ${leftToDo} items left to do.`); //affiche en nombre la constante leftToDo en nombre sur todos grâce 
