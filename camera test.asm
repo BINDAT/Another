8bd792c4540509b25271653bde68781f4c7fcf68
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-05-26 21:22:05 +0200

    Create camera test.asm

---
+++ camera test.asm
@@ -0,0 +1,25 @@
+; Serveur (camera_server.asm)
+section .data
+  ; ...
+
+section .bss
+  ; ...
+
+section .text
+  global _start
+
+_start:
+  ; ... (code pour la capture vidéo avec OpenCV et la communication réseau)
+
+; Client (camera_client.asm)
+section .data
+  ; ...
+
+section .bss
+  ; ...
+
+section .text
+  global _start
+
+_start:
+  ; ... (code pour la réception des images et l'affichage avec OpenCV)
