79c29284911273152bc2d1da4d0a02ce0eccaba2
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-05-26 21:21:37 +0200

    Create camera test.sh

---
+++ camera test.sh
@@ -0,0 +1,9 @@
+# Serveur (camera_server.sh)
+#!/bin/bash
+
+# ... (code pour la capture vidéo avec OpenCV et la communication réseau)
+
+# Client (camera_client.sh)
+#!/bin/bash
+
+# ... (code pour la réception des images et l'affichage avec OpenCV)
