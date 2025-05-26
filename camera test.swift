8ead33dcbf3ece94d6d88c59145772e8d91096de
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-05-26 21:19:58 +0200

    Create camera test.swift

---
+++ camera test.swift
@@ -0,0 +1,16 @@
+// Serveur (CameraServer.swift)
+import AVFoundation
+import Cocoa
+import Network
+
+class CameraServer: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    // ... (code pour la capture vidéo avec AVFoundation et la communication réseau)
+}
+
+// Client (CameraClient.swift)
+import Cocoa
+import Network
+
+class CameraClient: NSObject {
+    // ... (code pour la réception des images et l'affichage)
+}
