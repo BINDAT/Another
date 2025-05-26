56515efb9735d9e3424f5b63eee4af4f7c7b947b
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-05-26 21:19:36 +0200

    Create camera test.go

---
+++ camera test.go
@@ -0,0 +1,32 @@
+// Serveur (camera_server.go)
+package main
+
+import (
+	"image/jpeg"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"gocv.io/x/gocv/v2"
+)
+
+func main() {
+	// ... (code pour la capture vidéo avec GoCV et la communication réseau)
+}
+
+// Client (camera_client.go)
+package main
+
+import (
+	"image/jpeg"
+	"log"
+	"net"
+	"os"
+
+	"gocv.io/x/gocv/v2"
+)
+
+func main() {
+	// ... (code pour la réception des images et l'affichage avec GoCV)
+}
