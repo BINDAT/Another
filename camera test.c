bdbd1d8136e18596ac48059541dc73f39893a59a
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-05-26 21:16:42 +0200

    Create camera test.c

---
+++ camera test.c
@@ -0,0 +1,25 @@
+// Serveur (camera_server.c)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <opencv2/opencv.hpp>
+
+int main() {
+  // ... (code pour la capture vidéo avec OpenCV et la communication réseau)
+}
+
+// Client (camera_client.c)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <opencv2/opencv.hpp>
+
+int main() {
+  // ... (code pour la réception des images et l'affichage avec OpenCV)
+}
