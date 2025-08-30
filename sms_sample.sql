9bb515382f9417fd5162e32057fb4b742f1b1dc4
bindat <maximerigaud66@gmail.com>
2025-08-30 23:14:09 +0200

    Suppression des codes de test

--- sms_sample.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM sms WHERE address = '+33612345678';
-SELECT * FROM sms WHERE body LIKE '%anniversaire%';
--- sms_terminal.bash
+++
@@ -1,1 +0,0 @@
-python3 export_sms_html.py --xml /chemin/vers/backup.xml --out ./export
